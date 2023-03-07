a8d38e594a5e0a987b85b03c3099815624710b9f
Huzaifa0-2 <perfect.huzaifa01@gmail.com>
2023-03-07 16:50:08 +0500

    new commit

---
+++ task1.js
@@ -0,0 +1,4 @@
+var user = { name1: "Huzaifa", age: 20 };
+var a = 12;
+console.log(user.name1, user.age);
+console.log("\"NEW: \"".concat(user.name1, " ").concat(user.age));
---
+++ task1.ts
@@ -0,0 +1,5 @@
+let user = {name1: "Huzaifa", age: 20};
+let a= 12
+console.log(user.name1, user.age);
+
+console.log(`"NEW: "${user.name1} ${user.age}`);
